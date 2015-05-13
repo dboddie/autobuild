a76285174f89dcccf9c10ddfc3de267a79d63b35
David Boddie <david.boddie@met.no>
2015-05-13 12:31:23 +0200

    More colour tweaking.

--- autobuild-daemon.py
+++ autobuild-daemon.py
@@ -581,7 +581,7 @@ class Overview:
                 "<html>\n<head><title>$title</title>\n"
                 '<style type="text/css">\n'
                 '  .success { background-color: #c0f0c0; color: black }\n'
-                '  .started { background-color: #f0f080; color: black }\n'
+                '  .started { background-color: #f0c080; color: black }\n'
                 '  .failure { background-color: #f0c0c0; color: black }\n'
                 '  .commands { font-size: smaller }\n'
                 '  table { border: 1px solid #808080 }\n'
